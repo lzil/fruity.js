fb3372fe5ba3f451d9a7f0cc39b70fa09d47b1e9
Liang <zhoul@mit.edu>
2015-07-22 01:10:27 -0400

    more robust text replacement

--- fruity.js
+++ fruity.js
@@ -6,21 +6,44 @@ $(document).ready(function() {
 	'http://2.bp.blogspot.com/-7zl-8m8ksDY/UgCooHEW8dI/AAAAAAAAANs/2FxX3n5ai08/s1600/Strawberry.png',
 	'http://www.dhasmartbar.com/img_slider/slider4/blueberries_stack.png',
 	'http://bellvalefarms.net/images/slider/peach.png'
-	]
+	];
 	var images = document.getElementsByTagName('img');
 	for (var i = 0; i < images.length; i++) {
+		var h = images[i].style.height;
+		var w = images[i].style.width;
+		images[i].style.maxHeight = h;
+		images[i].style.maxWidth = w;
 		images[i].src = fruitImages[Math.floor(Math.random()*fruitImages.length)];
-	}
+	};
 
-	console.log(document.body.textContent.split("\\s+"));
-	document.body.innerHTML = document.body.innerHTML.replace(/ the /ig, ' apple ');
-	document.body.innerHTML = document.body.innerHTML.replace(/ a /ig, ' orange ');
-	document.body.innerHTML = document.body.innerHTML.replace(/ I /ig, ' kiwi ');
-	document.body.innerHTML = document.body.innerHTML.replace(/ and /ig, ' mango ');
+	fruitWords = ['apple', 'orange', 'kiwi', 'mango', 'grape', 'pineapple', 'watermelon', 'papaya', 'peach', 'cantaloupe', 'banana', 'cherries'];
+	
+	var children = document.body.childNodes;
+	var ind = 0;
+	children = Array.prototype.slice.call(children);
+	while (ind < children.length) {
+		if (children[ind].nodeType == 3) {
+			children[ind].textContent = children[ind].textContent.replace(/ the | a | I | and /ig, ' ' + fruitWords[Math.floor(Math.random()*fruitWords.length)] + ' ')
+		} else {
+			var nextChildren = children[ind].childNodes;
+			nextChildren = Array.prototype.slice.call(nextChildren);
+			if (nextChildren.length > 0) {
+				for (var i = 0; i < nextChildren.length; i++) {
+					children.push(nextChildren[i]);
+				}
+			}
+		}
+		ind++;
+	}
+	
+	// document.body.innerHTML = document.body.innerHTML.replace(/ the /ig, ' apple ');
+	// document.body.innerHTML = document.body.innerHTML.replace(/ a /ig, ' orange ');
+	// document.body.innerHTML = document.body.innerHTML.replace(/ I /ig, ' kiwi ');
+	// document.body.innerHTML = document.body.innerHTML.replace(/ and /ig, ' mango ');
 
 	var links = document.getElementsByTagName('a');
 	for (var i = 0; i < links.length; i++) {
-		links[i].href = 'https://en.wikipedia.org/wiki/Fruit'
+		links[i].href = 'https://en.wikipedia.org/wiki/Fruit';
 	};
 
 })
